c58e2f6bce31488f5ac1599e947ca75254b1b6ba
foxelas <32893423+foxelas@users.noreply.github.com>
2020-08-18 14:57:01 +0900

    Add files via upload

---
+++ table2latex.m
@@ -0,0 +1,61 @@
+function Ttex = table2latex(T, selectedCols)
+%     TABLE2LATEX converts a table to the tabular form for use in LaTeX. 
+% 
+%     Input args 
+%     T: a table of elements 
+%     selectedCols: index vector of table columns that need to be printed
+% 
+%     Output args 
+%     Ttex: the formatted text for the table 
+% 
+%     Dependencies 
+%     None
+% 
+%     Usage 
+%     Ttex = table2latex(T);
+%     Ttex = table2latex(T, [1:5]);
+%     The function prints the formatted table, which then
+%     can be copied and pasted to LaTeX as is. 
+
+
+v = table2cell(T);
+rows = size(v, 1);
+
+if nargin < 2
+    columns = size(v, 2);
+    selectedCols = 1:columns;
+else 
+    columns = numel(selectedCols);
+end 
+
+symb = ' & ';
+slant = '\\';
+textRows = cell(2 + rows + 1, 1);
+textRows{1} = strcat(slant, 'begin{table}[htb]\n', slant, 'caption{ }\n', slant, 'begin{center}\n', slant, 'label{tab: }\n{', slant, 'tt');
+textRows{2} = strcat(slant, 'begin{tabular}{|');
+for ii = 1:columns
+    textRows{2} = strcat(textRows{2} , 'c|') ;
+end
+textRows{2} = strcat(textRows{2},  '}', slant, 'hline');
+
+for ii = 1:rows
+    textRows{ii + 2} = strcat(strjoin(cellfun(@(x) convertCell(x), v(ii,selectedCols),  'UniformOutput', false), symb), slant, slant, slant, 'hline');
+end
+
+textRows{rows+3} = strcat(slant, 'end{tabular}\n}\n', slant, 'end{center}\n', slant, 'end{table}', '\n');
+
+Ttex = strjoin(textRows, '\n');
+fprintf(Ttex);
+
+end 
+
+function xnew = convertCell(x)
+    if ischar(x)
+        xnew = x;
+    elseif isnumeric(x)
+        xnew = num2str(x, '%.3f');
+    elseif islogical(x)
+        logVals = {'F', 'T'};
+        xnew = logVals{x + 1};
+    end 
+end
