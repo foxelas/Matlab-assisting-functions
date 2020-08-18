5883c1564b2584cf3cd44f278784ed0296bd566f
foxelas <32893423+foxelas@users.noreply.github.com>
2020-08-18 17:50:02 +0900

    add caption and label args

--- table2latex.m
+++ table2latex.m
@@ -1,4 +1,4 @@
-function Ttex = table2latex(T, selectedCols)
+function Ttex = table2latex(T, selectedCols, label, caption)
 %     TABLE2LATEX converts a table to the tabular form for use in LaTeX. 
 % 
 %     Input args 
@@ -28,10 +28,18 @@ else
     columns = numel(selectedCols);
 end 
 
+if nargin < 3 
+    label = '';
+end 
+
+if nargin < 4 
+    caption = '';
+end 
+
 symb = ' & ';
 slant = '\\';
 textRows = cell(2 + rows + 1, 1);
-textRows{1} = strcat(slant, 'begin{table}[htb]\n', slant, 'caption{ }\n', slant, 'begin{center}\n', slant, 'label{tab: }\n{', slant, 'tt');
+textRows{1} = strcat(slant, 'begin{table}[htb]\n', slant, 'caption{', caption,'}\n', slant, 'begin{center}\n', slant, 'label{tab:', label, '}\n{', slant, 'tt');
 textRows{2} = strcat(slant, 'begin{tabular}{|');
 for ii = 1:columns
     textRows{2} = strcat(textRows{2} , 'c|') ;
@@ -44,7 +52,7 @@ for ii = 1:rows
     textRows{ii + 3} = strcat(strjoin(cellfun(@(x) convertCell(x), v(ii,selectedCols),  'UniformOutput', false), symb), slant, slant, slant, 'hline');
 end
 
-textRows{rows+4} = strcat(slant, 'end{tabular}\n}\n', slant, 'end{center}\n', slant, 'end{table}', '\n');
+textRows{rows+4} = strcat(slant, 'end{tabular}\n}\n', slant, 'end{center}\n', slant, 'end{table}', '\n\n');
 
 Ttex = strjoin(textRows, '\n');
 fprintf(Ttex);
