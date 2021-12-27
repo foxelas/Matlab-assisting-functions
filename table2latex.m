efa81e9aa6b00a22062a8578346e3878545b26a4
foxelas <32893423+foxelas@users.noreply.github.com>
2021-12-27 15:01:31 +0900

    Update table2latex.m

--- table2latex.m
+++ table2latex.m
@@ -1,4 +1,4 @@
-function Ttex = table2latex(T, selectedCols, label, caption)
+function Ttex = table2latex(T, selectedCols, label, caption, colWidths, isLandscape)
 %     table2latex converts a table to the tabular form for use in LaTeX.
 %
 %     Input args
@@ -44,54 +44,110 @@ if nargin < 4
     caption = '';
 end
 
+if nargin < 5 
+    colWidths = [];
+end
+
+if nargin < 6 
+    isLandscape = false;
+end
+
 symb = ' & ';
 slant = '\\';
 textRows = cell(2+rows+1, 1);
-textRows{1} = strcat(slant, 'begin{table}[htb]\n', slant, 'caption{', caption, '}\n', slant, 'begin{center}\n', slant, 'label{tab:', label, '}\n{', slant, 'tt');
-textRows{2} = strcat(slant, 'begin{tabular}{|');
-for ii = 1:columns
-    textRows{2} = strcat(textRows{2}, 'c|');
+curRows = 1;
+
+if isLandscape 
+    textRows{curRows} = strcat(slant, 'begin{landscape}\n');
+    curRows = curRows + 1;
+end 
+textRows{curRows} = strcat(slant, 'begin{table}[htb]\n', slant, 'caption{', caption, '}\n', slant, 'begin{center}\n', slant, 'label{tab:', label, '}\n{', slant, 'tt');
+curRows = curRows + 1; 
+textRows{curRows} = strcat(slant, 'begin{tabular}{|');
+
+
+if length(colWidths) ~= columns
+    disp('Incorrect number of column widths');
 end
-textRows{2} = strcat(textRows{2}, '}', slant, 'hline');
+
+for ii = 1:columns
+    if isempty(colWidths)
+        textRows{curRows} = strcat(textRows{curRows}, 'c|');
+    else
+        textRows{curRows} = strcat(textRows{curRows}, 'p{', num2str(colWidths(ii)), 'cm}|');
+    end
+end
+textRows{curRows} = strcat(textRows{curRows}, '}', slant, 'hline');
 
 if hasHeader
-    textRows{3} = strcat(strjoin(cellfun(@(x) convertCell(x), T.Properties.VariableNames(selectedCols), 'UniformOutput', false), symb), slant, slant, slant, 'hline');
-    curRows = 3;
-else
-    curRows = 2;
+    curRows = curRows + 1;
+    textRows{curRows} = strcat(strjoin(cellfun(@(x) convertCell(x, hasHeader), T.Properties.VariableNames(selectedCols), 'UniformOutput', false), symb), slant, slant, slant, 'hline');
 end
 
+hasExtraHeader = false; 
 for ii = 1:rows
     curRows = curRows + 1;
-    textRows{curRows} = strcat(strjoin(cellfun(@(x) convertCell(x), v(ii, selectedCols), 'UniformOutput', false), symb), slant, slant, slant, 'hline');
+    if sum(cellfun(@(x) isempty(x) | sum(isnan(x)) > 0, v(ii, selectedCols))) == numel(v(ii, selectedCols))
+        hasExtraHeader = true; 
+        textRows{curRows} = '';
+    elseif hasExtraHeader
+        textRows{curRows} = strcat(slant, 'hline');
+        curRows = curRows + 1;
+        textRows{curRows} = strcat(strjoin(cellfun(@(x) convertCell(x, true), v(ii, selectedCols), 'UniformOutput', false), symb), slant, slant, slant, 'hline');
+        curRows = curRows + 1;
+        textRows{curRows} = strcat(slant, 'hline');
+        hasExtraHeader = false;
+    else
+        textRows{curRows} = strcat(strjoin(cellfun(@(x) convertCell(x), v(ii, selectedCols), 'UniformOutput', false), symb), slant, slant, slant, 'hline');
+    end
 end
 
 curRows = curRows + 1;
 textRows{curRows} = strcat(slant, 'end{tabular}\n}\n', slant, 'end{center}\n', slant, 'end{table}', '\n\n');
+
+if isLandscape 
+    curRows = curRows + 1;
+    textRows{curRows} = strcat(slant, 'end{landscape}\n');
+end 
 
 Ttex = strjoin(textRows, '\n');
 fprintf(Ttex);
 
 end
 
-function xnew = convertCell(x)
-if ischar(x) && ~isnan(str2double(x))
-    xnew = num2str(str2double(x) * 100, '%.2f');
-    xnew = strcat(xnew, '\\%%');
+function xnew = convertCell(x, hasHeader)
+if nargin < 2 
+    hasHeader = false;
+end 
+if ~isempty(x) && ~strcmp(x, '')
+    if strcmpi(x, 'na') | isnan(x)
+        xnew = 'NA';
         
-elseif ischar(x)
-    xnew = strrep(x, '%', '\\%%'); 
-    xnew = strrep(xnew, '&', '\\&');
-    xnew = strrep(xnew, '_', '\\_');
-    xnew = strrep(xnew, '\cite', '\\cite');
-elseif isnumeric(x)
-    if floor(x)==x
-        xnew = num2str(x, '%d');
-    else
-        xnew = num2str(x, '%.3f');
+    elseif ischar(x) && ~isnan(str2double(x))
+        xnew = num2str(str2double(x) * 100, '%.2f');
+        xnew = strcat(xnew, '\\%%');
+
+    elseif ischar(x)
+        xnew = strrep(x, '%', '\\%%'); 
+        xnew = strrep(xnew, '&', '\\&');
+        xnew = strrep(xnew, '_', '\\_');
+        xnew = strrep(xnew, '\cite', '\\cite');
+    elseif isnumeric(x)
+        if floor(x)==x
+            xnew = num2str(x, '%d');
+        else
+            xnew = num2str(x, '%.3f');
+        end
+    elseif islogical(x)
+        logVals = {'F', 'T'};
+        xnew = logVals{x+1};
     end
-elseif islogical(x)
-    logVals = {'F', 'T'};
-    xnew = logVals{x+1};
+
+    if hasHeader 
+        xnew = strcat('\\', 'textbf{', xnew , '}');
+    end
+else 
+    xnew = ' ';
 end
+
 end
